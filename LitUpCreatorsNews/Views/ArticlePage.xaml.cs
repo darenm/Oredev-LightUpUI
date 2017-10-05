fcfc3c5df3f468aae64bfccce22867e365456aae
Daren May <daren.may@gmail.com>
2017-10-05 13:00:29 -0600

    Connected animation running

--- LitUpCreatorsNews/Views/ArticlePage.xaml.cs
+++ LitUpCreatorsNews/Views/ArticlePage.xaml.cs
@@ -1,5 +1,11 @@
-﻿using Windows.UI.Core;
+﻿using System;
+using Windows.UI.Composition;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -11,15 +17,57 @@ namespace CreatorsNews.Views
     /// </summary>
     public sealed partial class ArticlePage : Page
     {
+        private static ScalarKeyFrameAnimation OpacityAnimation;
+        private Visual _articleVisual;
+
         public ArticlePage()
         {
             InitializeComponent();
+            EnsureAnimation();
+            _articleVisual = ElementCompositionPreview.GetElementVisual(ArticlePagePanel);
+            _articleVisual.Opacity = 0;
+        }
+
+        private void EnsureAnimation()
+        {
+            if (OpacityAnimation != null)
+            {
+                return;
+            }
+
+            var linear = Window.Current.Compositor.CreateLinearEasingFunction();
+            OpacityAnimation = Window.Current.Compositor.CreateScalarKeyFrameAnimation();
+            OpacityAnimation.InsertKeyFrame(0, 0);
+            OpacityAnimation.InsertKeyFrame(0.2f, 0, linear);
+            OpacityAnimation.InsertKeyFrame(1, 1, linear);
+            OpacityAnimation.Duration = TimeSpan.FromMilliseconds(600);
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             var articleIndex = (int) e.Parameter;
             ViewModel.LoadArticle(articleIndex);
+
+            BackgroundImage.Source = ViewModel.Article.MainImage;
+
+            var animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("Image");
+            if (animation != null)
+            {
+                BackgroundImage.Opacity = 0;
+                // Wait for image opened. In future Insider Preview releases, this won't be necessary.
+                BackgroundImage.ImageOpened += (sender_, e_) =>
+                {
+                    BackgroundImage.Opacity = 1;
+                    animation.TryStart(BackgroundImage);
+                    _articleVisual.StartAnimation(nameof(Visual.Opacity), OpacityAnimation);
+                };
+            }
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("Image", BackgroundImage);
+            base.OnNavigatingFrom(e);
         }
     }
 }
--- LitUpCreatorsNews/Views/ArticlesPage.xaml.cs
+++ LitUpCreatorsNews/Views/ArticlesPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Core;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -23,15 +25,33 @@ namespace CreatorsNews.Views
     /// </summary>
     public sealed partial class ArticlesPage : Page
     {
+        static int? _articleIndex;
         public ArticlesPage()
         {
             this.InitializeComponent();
+            ArticlesGrid.Loaded += this.ArticlesGrid_Loaded;
         }
+
 
         private void ArticleClicked(object sender, ItemClickEventArgs e)
         {
-            var articleIndex = Array.IndexOf(ViewModel.Articles, e.ClickedItem);
-            Frame.Navigate(typeof(Views.ArticlePage), articleIndex);
+            _articleIndex = Array.IndexOf(ViewModel.Articles, e.ClickedItem);
+
+
+            var container = ArticlesGrid.ContainerFromItem(e.ClickedItem) as GridViewItem;
+            if (container != null)
+            {
+                var root = (FrameworkElement)container.ContentTemplateRoot;
+                var image = (UIElement)root.FindName("Image");
+
+                ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("Image", image);
+            }
+
+            // Add a fade out effect
+            Transitions = new TransitionCollection();
+            Transitions.Add(new ContentThemeTransition());
+
+            Frame.Navigate(typeof(Views.ArticlePage), _articleIndex);
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -45,5 +65,48 @@ namespace CreatorsNews.Views
             }
 
         }
+
+        private async void ArticlesGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (_articleIndex != null)
+            {
+                // May be able to perform backwards Connected Animation
+                var animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("Image");
+                if (animation != null)
+                {
+                    var item = ViewModel.Articles[_articleIndex.Value];
+
+                    ArticlesGrid.ScrollIntoView(item, ScrollIntoViewAlignment.Default);
+                    ArticlesGrid.UpdateLayout();
+
+                    // wait for the scroll into view to complete
+                    await Task.Delay(TimeSpan.FromMilliseconds(100));
+
+                    var container = ArticlesGrid.ContainerFromItem(item) as GridViewItem;
+                    if (container != null)
+                    {
+                        var root = (FrameworkElement)container.ContentTemplateRoot;
+                        var image = (Image)root.FindName("Image");
+
+                        //// Wait for image opened. In future Insider Preview releases, this won't be necessary.
+                        //image.Opacity = 0;
+                        //image.ImageOpened += (sender_, e_) =>
+                        //{
+                        //    image.Opacity = 1;
+                        //};
+
+                        animation.TryStart(image);
+
+                    }
+                    else
+                    {
+                        animation.Cancel();
+                    }
+                }
+
+                _articleIndex = null;
+            }
+        }
+
     }
 }
