3006fd5b720c84546db943b47a00c657f7f58063
Daren May <daren.may@gmail.com>
2017-11-02 10:35:31 -0600

    Added an altenate article view for the News page

--- LitUpCreatorsNews/Views/NewsPage.xaml.cs
+++ LitUpCreatorsNews/Views/NewsPage.xaml.cs
@@ -1,6 +1,14 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Windows.UI.Composition;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using CreatorsNews.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -11,15 +19,63 @@ namespace CreatorsNews.Views
     /// </summary>
     public sealed partial class NewsPage : Page
     {
+        private static int? _articleIndex;
+
         public NewsPage()
         {
             InitializeComponent();
+            ArticlesGrid.Loaded += ArticlesGrid_Loaded;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            var animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("Image");
-            animation?.Cancel();
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = Frame.CanGoBack
+                ? AppViewBackButtonVisibility.Visible
+                : AppViewBackButtonVisibility.Collapsed;
+
+            // Don't use vertical entrance animation with connected animation
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                EntranceTransition.FromVerticalOffset = 0;
+            }
+        }
+
+
+        private void ArticleClicked(object sender, ItemClickEventArgs e)
+        {
+            _articleIndex = Array.IndexOf(ViewModel.Articles, e.ClickedItem as Article);
+            ArticlesGrid.PrepareConnectedAnimation("Image", e.ClickedItem, "Image");
+
+            // Add a fade out effect
+            Transitions = new TransitionCollection { new ContentThemeTransition() };
+
+            Frame.Navigate(typeof(NewsArticlePage), _articleIndex);
+        }
+
+        private async void ArticlesGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (_articleIndex != null)
+            {
+                // May be able to perform backwards Connected Animation
+                var animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("Image");
+                if (animation != null)
+                {
+                    var item = ViewModel.Articles[_articleIndex.Value];
+                    if (item != null)
+                    {
+                        ArticlesGrid.ScrollIntoView(item, ScrollIntoViewAlignment.Default);
+                        // wait for scroll
+                        await Task.Delay(TimeSpan.FromMilliseconds(100));
+                        await ArticlesGrid.TryStartConnectedAnimationAsync(animation, item, "Image");
+                    }
+                    else
+                    {
+                        animation.Cancel();
+                    }
+                }
+
+                _articleIndex = null;
+            }
         }
     }
 }
